bf30fc36b03be41a172cd27973e4d01ba8da9788
priyanshusingh305 <singhpriyanshu305@gmail.com>
2022-03-24 16:19:59 +0530

    😅

--- App/src/App.js
+++ App/src/App.js
@@ -14,6 +14,18 @@ function App() {
       isPlaying={isPlaying}
       setIsPlaying={setIsPlaying}
       currentSong={currentSong}/>
+      {/* <footer>
+       <p class="mt-5 text-slate-400">
+                Made with 💖 by
+                <a href="https://www.linkedin.com/in/priyanshu-singh-a051591b3/" class="text-slate-500 dark:text-white" target="_blank" rel="noopener"
+                    > Priyanshu Singh</a>.
+            </p>
+            <p class="mb-5 mt-1 text-slate-400 text-xs">
+                Copyright &copy; Priyanshu Developer. Licensed under
+                <a class="text-slate-500 dark:text-white" href="https://github.com/priyanshusingh305/react-player/blob/main/LICENSE"
+                    > GPL-v2</a>.
+            </p>
+        </footer>             */}
     </div>
   );
 }
