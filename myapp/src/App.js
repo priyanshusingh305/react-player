e7b3343a12953e9a26f2c6bbe49c4a7e058fef8f
priyanshusingh305 <singhpriyanshu305@gmail.com>
2022-11-29 18:39:28 +0530

    ui fix

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
   //Ref
   const audioRef =useRef(null);
   //state
-  const [songs,setSong]=useState(data());
+  const [songs,setSongs]=useState(data());
   const [currentSong,setCurrentSong]=useState(songs[5]);
   const[isPlaying,setIsPlaying]=useState(false)
   const[songInfo,setSongInfo]=useState({
@@ -34,7 +34,7 @@ function App() {
         songInfo={songInfo}
 
       />
-      <Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying}/>
+      <Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} setSongs={setSongs}/>
       <audio 
         onTimeUpdate={timeUpdateHandler}
         onLoadedMetadata={timeUpdateHandler}
--- myapp/src/components/LibrarySong.js
+++ myapp/src/components/LibrarySong.js
@@ -1,9 +1,24 @@
 import React from "react";
-const LibrarySong =({song,setCurrentSong,songs,id,audioRef,isPlaying})=>{
+const LibrarySong =({song,setCurrentSong,songs,id,audioRef,isPlaying, setSongs})=>{
     const songSelectHandler=()=>{
         const selectedSong=songs.filter((state)=>state.id===id);
         setCurrentSong(selectedSong[0]);
-        // audioRef.current.play();
+        // Add Active State
+        const newSongs=songs.map((song)=>{
+            if(song.id===id){
+                return{
+                    ...song,
+                    active:true,
+                }
+            }
+            else{
+                return{
+                    ...song,
+                    active:false,
+                }
+            }
+        });
+        setSongs(newSongs);
         // check if the song is playing
         if(isPlaying){
             const playPromise=audioRef.current.play();
@@ -16,7 +31,7 @@ const LibrarySong =({song,setCurrentSong,songs,id,audioRef,isPlaying})=>{
     };
     return(
    
-        <div onClick={songSelectHandler} className='library-song'>
+        <div onClick={songSelectHandler} className={`library-song ${song.active?'selected':""}`}>
         <img alt={song.name} src={song.cover}></img>
         <div className="song-description">
         <h3>{song.name}</h3>
