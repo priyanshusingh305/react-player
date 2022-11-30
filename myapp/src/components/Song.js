b2b980ebe156eb4f5b2f881e9e821bf47ffa4641
priyanshusingh305 <singhpriyanshu305@gmail.com>
2022-11-30 18:55:25 +0530

    odd shadow

--- myapp/src/components/Song.js
+++ myapp/src/components/Song.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Song =({currentSong})=>{
     return(
         <div className='song-container'>
-        <img alt={currentSong.name} src={currentSong.cover}></img>
+        <img  style={{boxShadow:`5px 5px 5px 1px ${currentSong.color[1]}` }}  alt={currentSong.name} src={currentSong.cover}></img>
         <h2>{currentSong.name}</h2>
         <h3>{currentSong.artist}</h3>
         </div>
